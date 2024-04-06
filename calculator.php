7241a0ba60be1e33d75ba2b4d3068cfb5c85ac69
CounterDark <aleksanderkunkowski@gmail.com>
2024-04-06 16:26:25 +0200

    calculator done

--- calculator.php
+++ calculator.php
@@ -1,3 +1,26 @@
 <?php 
-    
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator = $_POST['operator'];
+    switch ($operator) {
+        case 'add':
+            echo $num1 + $num2;
+            break;
+        case 'subtract':
+            echo $num1 - $num2;
+            break;
+        case 'multiply':
+            echo $num1 * $num2;
+            break;
+        case 'divide':
+            if($num2 == 0){
+                echo "Cannot divide by zero";
+                break;
+            }
+            echo $num1 / $num2;
+            break;
+        default:
+            echo "Invalid operator";
+            break;
+    }
 ?>
