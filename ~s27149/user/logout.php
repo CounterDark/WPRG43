9476f7b487edb7df3d295a980436599ea357df29
CounterDark <aleksanderkunkowski@gmail.com>
2024-06-28 18:02:39 +0200

    added main menu initial structure and logout

---
+++ ~s27149/user/logout.php
@@ -0,0 +1,17 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!isset($_SESSION['db_host']) || !isset($_SESSION['db_user']) || !isset($_SESSION['db_pass'])) {
+    header('Location: ../index.php');
+    exit();
+}
+
+if (!isset($_SESSION['logged_in']) || (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == false)) {
+    header('Location: ../index.php');
+    exit();
+}
+
+include_once './logic/logout_user.php';
+?>
